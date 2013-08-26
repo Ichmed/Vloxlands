9f69d459f36a9f22006b303853842e03171fff68
Ichmed <mijomeyer@t-online.de>
2013-08-26 14:37:23 +0200

    changed island List to HashMap

--- src/com/vloxlands/game/world/Map.java
+++ src/com/vloxlands/game/world/Map.java
@@ -1,13 +1,14 @@
 package com.vloxlands.game.world;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.vloxlands.game.entity.Entity;
 
 public class Map
 {
-	List<Island> islands = new ArrayList<>();
+	HashMap<Integer, Island> islands = new HashMap<>();
 	List<Entity> entities = new ArrayList<>();
 	
 	public void placeVoxel(Island i, int x, int y, int z, int id)
