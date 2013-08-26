ac1f60c2061283e93f08b8164d05c925a6760091
Ichmed <mijomeyer@t-online.de>
2013-08-26 13:43:41 +0200

    added setVoxel
    also added metadata param

--- src/com/vloxlands/game/world/Island.java
+++ src/com/vloxlands/game/world/Island.java
@@ -1,12 +1,35 @@
 package com.vloxlands.game.world;
 
+import com.vloxlands.game.voxel.Voxel;
+
 public class Island
 {
 	int[][][] voxels = new int[65536][65536][65536];
-	int[][][] metadata = new int[256][256][256];
+	int[][][] voxelMetadata = new int[256][256][256];
 	
 	public void placeVoxel(int x, int y, int z, int id)
 	{
 		voxels[x][y][z] = id;
+		voxelMetadata[x][y][z] = 0;
+		Voxel.getVoxelForId(id).onPlaced(x, y, z);		
+	}
+
+	public void placeVoxel(int x, int y, int z, int id, int metadata)
+	{
+		voxels[x][y][z] = id;
+		voxelMetadata[x][y][z] = metadata;
+		Voxel.getVoxelForId(id).onPlaced(x, y, z);		
+	}
+	
+	public void setVoxel(int x, int y, int z, int id)
+	{
+		voxels[x][y][z] = id;
+		voxelMetadata[x][y][z] = 0;
+	}
+	
+	public void setVoxel(int x, int y, int z, int id, int metadata)
+	{
+		voxels[x][y][z] = id;
+		voxelMetadata[x][y][z] = metadata;
 	}
 }
