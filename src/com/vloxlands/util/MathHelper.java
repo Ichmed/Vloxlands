6905bc3e14a5b19515dc0d9d731c5037ac966a3c
Ichmed <mijomeyer@t-online.de>
2013-08-27 15:59:50 +0200

    added asFloatBuffer

--- src/com/vloxlands/util/MathHelper.java
+++ src/com/vloxlands/util/MathHelper.java
@@ -1,9 +1,21 @@
 package com.vloxlands.util;
 
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+
 public class MathHelper
 {
 	public static float clamp(float x, int i, int j)
 	{
 		return Math.max(i, Math.min(x, j));
+	}
+	
+	public static FloatBuffer asFloatBuffer(float[] fs)
+	{
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(fs.length);
+		buffer.put(fs);
+		buffer.flip();
+		return buffer;
 	}
 }
