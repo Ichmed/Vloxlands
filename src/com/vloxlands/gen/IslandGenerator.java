40d9993ed35eb400655123a75c1a36104220b7f7
Dakror <mail@dakror.de>
2013-08-27 16:02:05 +0200

    I can't see nothin! :(

--- src/com/vloxlands/gen/IslandGenerator.java
+++ src/com/vloxlands/gen/IslandGenerator.java
@@ -10,12 +10,18 @@ public class IslandGenerator
 {
 	public static Island generatePerfectIsland()
 	{
+		long time = System.currentTimeMillis();
+		
 		short topLayers = (short) (Math.random() * 3 + 3);
 		short tailLayers = (short) (Math.random() * 8 + 8);
-		short radius = (short) (Math.random() * 9 + 32);
+		// short radius = (short) (Math.random() * 16);
 		Island island = new Island();
 		
-		fillHorizontalCircle((short) 0, radius, Voxel.GRASS.getId(), island);
+		for (int i = 0; i < 1; i++)
+		{
+			fillHorizontalCircle((short) i, (short) 4, Voxel.GRASS.getId(), island);
+		}
+		CFG.p("[IslandGenerator]: Generation took " + (System.currentTimeMillis() - time) + "ms");
 		
 		return island;
 		
@@ -23,20 +29,24 @@ public class IslandGenerator
 	
 	private static void fillHorizontalCircle(short h, short radius, byte b, Island island)
 	{
-		Vector2f center = new Vector2f(radius, radius);
 		
-		int c = 0;
+		int x = (int) (Island.MAXSIZE / 2f - radius);
+		int z = (int) (Island.MAXSIZE / 2f - radius);
+		CFG.p("radius: " + radius);
+		Vector2f center = new Vector2f(radius + x, radius + z);
 		
-		for (short i = 0; i < radius * 2; i++) // x axis
+		for (short i = (short) x; i < radius * 2 + x; i++) // x axis
 		{
-			for (short j = 0; j < radius * 2; j++) // z axis
+			for (short j = (short) z; j < radius * 2 + z; j++) // z axis
 			{
+				
 				Vector2f distance = Vector2f.sub(center, new Vector2f(i, j), null);
-				if (distance.length() <= radius) island.setVoxel(i, h, j, b);
-				c++;
+				if (distance.length() < radius)
+				{
+					CFG.p(i + ":" + j);
+					island.setVoxel(i, h, j, b);
+				}
 			}
 		}
-		
-		CFG.p(c);
 	}
 }
