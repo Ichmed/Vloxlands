4ba1f5e5de0929f53336d823f07f694eba6552b1
Ichmed <mijomeyer@t-online.de>
2013-08-29 14:41:15 +0200

    some textures and working on IslandRenderer

--- src/com/vloxlands/render/IslandRenderer.java
+++ src/com/vloxlands/render/IslandRenderer.java
@@ -1,13 +1,26 @@
 package com.vloxlands.render;
 
+import java.nio.IntBuffer;
 import java.util.HashMap;
 
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL15;
 import org.lwjgl.util.vector.Vector4f;
+
+import com.vloxlands.game.world.Island;
 
 public class IslandRenderer
 {	
 	public HashMap<Vector4f, VoxelFace> faces = new HashMap<>();
 	public HashMap<Vector4f, VoxelFace> transparentFaces = new HashMap<>();
 	
+	
+	
+	public static void createVBOID()
+	{
+		IntBuffer buffer = BufferUtils.createIntBuffer((int) (Math.pow(Island.MAXSIZE, 3) * 4));
+		GL15.glGenBuffers(buffer);		
+	}
+
 	
 }
