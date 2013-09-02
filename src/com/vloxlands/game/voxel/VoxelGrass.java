4d27ff8ae2507a9d0f55c250f0d30c2679c4339b
Ichmed <mijomeyer@t-online.de>
2013-09-02 18:04:22 +0200

    worked on grass

--- src/com/vloxlands/game/voxel/VoxelGrass.java
+++ src/com/vloxlands/game/voxel/VoxelGrass.java
@@ -1,5 +1,17 @@
 package com.vloxlands.game.voxel;
 
+import com.vloxlands.util.Direction;
+
 public class VoxelGrass extends Voxel
 {
+
+	@Override
+	public int getTextureIndex(int x, int y, int z, int d, int meta)
+	{
+		if(d == Direction.UP.ordinal()) return textureIndex - 32;
+		if(d == Direction.DOWN.ordinal()) return textureIndex + 64;
+		return textureIndex;
+		
+	}
+	
 }
