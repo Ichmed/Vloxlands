27633175e88eb82f3a8bc83045ba458d18d102d5
Dakror <mail@dakror.de>
2013-09-04 20:50:38 +0200

    Forgot new file :/

---
+++ src/com/vloxlands/game/UpdateThread.java
@@ -0,0 +1,112 @@
+package com.vloxlands.game;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+
+import com.vloxlands.Vloxlands;
+import com.vloxlands.render.ChunkRenderer;
+import com.vloxlands.settings.CFG;
+import com.vloxlands.settings.Settings;
+import com.vloxlands.util.RenderAssistant;
+
+/**
+ * @author Dakror
+ */
+public class UpdateThread extends Thread
+{
+	public static UpdateThread currentUpdateThread;
+	int ticks;
+	
+	public UpdateThread()
+	{
+		currentUpdateThread = this;
+		ticks = 0;
+		setDaemon(true);
+		setName("Update Thread");
+		start();
+	}
+	
+	@Override
+	public void run()
+	{
+		while (!Display.isCloseRequested())
+		{
+			
+			if (Game.currentGame.scene != null && Game.currentGame.scene.initialized) Game.currentGame.scene.onTick();
+			
+			while (Keyboard.next())
+			{
+				if (Keyboard.getEventKey() == Keyboard.KEY_F11 && !Keyboard.getEventKeyState())
+				{
+					CFG.FULLSCREEN = !CFG.FULLSCREEN;
+					Vloxlands.setFullscreen();
+					Game.currentGame.updateViewport();
+					Settings.saveSettings();
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_F4 && !Keyboard.getEventKeyState())
+				{
+					Game.currentGame.frames = 0;
+					CFG.SHOW_DEBUG = !CFG.SHOW_DEBUG;
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_L && !Keyboard.getEventKeyState()) CFG.LIGHTING = !CFG.LIGHTING;
+				if (Keyboard.getEventKey() == Keyboard.KEY_B && !Keyboard.getEventKeyState()) CFG.SHOW_CHUNK_BOUNDRIES = !CFG.SHOW_CHUNK_BOUNDRIES;
+				if (Keyboard.getEventKey() == Keyboard.KEY_T && Keyboard.getEventKeyState()) ChunkRenderer.renderChunks(Game.currentMap.islands.get(0));
+				if (Keyboard.getEventKey() == Keyboard.KEY_Z && Keyboard.getEventKeyState())
+				{
+					CFG.SHOW_WIREFRAME = !CFG.SHOW_WIREFRAME;
+					ChunkRenderer.renderChunks(Game.currentMap.islands.get(0));
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_V && !Keyboard.getEventKeyState())
+				{
+					CFG.SHOW_DIRECTIONS = !CFG.SHOW_DIRECTIONS;
+					Game.currentGame.directionalArrowsPos = new Vector3f(Game.currentGame.camera.getPosition());
+				}
+				if (Game.currentGame.scene != null) Game.currentGame.scene.handleKeyboard(Keyboard.getEventKey(), Keyboard.getEventKeyState());
+			}
+			
+			
+			if (Keyboard.isKeyDown(Keyboard.KEY_R))
+			{
+				Game.currentGame.lightPos.x = Game.currentGame.camera.position.x;
+				Game.currentGame.lightPos.y = Game.currentGame.camera.position.y;
+				Game.currentGame.lightPos.z = Game.currentGame.camera.position.z;
+				RenderAssistant.setUniform3f("lightPosition", Game.currentGame.lightPos);
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_UP))
+			{
+				Game.currentGame.lightPos.z++;
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+			{
+				Game.currentGame.lightPos.z--;
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
+			{
+				Game.currentGame.lightPos.x++;
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
+			{
+				Game.currentGame.lightPos.x--;
+			}
+			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) Game.currentGame.cameraSpeed = 0.5f;
+			else Game.currentGame.cameraSpeed = 0.3f;
+			
+			
+			try
+			{
+				ticks++;
+				Thread.sleep(20);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public int getTicksPS()
+	{
+		return Math.round(ticks / ((System.currentTimeMillis() - Game.currentGame.start) / 1000f));
+	}
+}
