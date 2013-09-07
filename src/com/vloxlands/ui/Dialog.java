182d0af131e881bc90615699cd2b31b1b5eb7080
Dakror <mail@dakror.de>
2013-09-07 15:10:20 +0200

    Fixed Dialog resize bug

--- src/com/vloxlands/ui/Dialog.java
+++ src/com/vloxlands/ui/Dialog.java
@@ -37,34 +37,16 @@ public class Dialog extends Scene
 	public Font titleFont = FontAssistant.GAMEFONT.deriveFont(Font.BOLD, 30f);
 	public Font messageFont = FontAssistant.GAMEFONT.deriveFont(Font.PLAIN, 30f);
 	
+	String rawTitle, rawMessage;
+	Action[] rawActions;
+	
 	public Dialog(String title, String message, Action... buttons)
 	{
-		this.title = title;
-		centered = true;
+		rawTitle = title;
+		rawMessage = message;
+		rawActions = buttons;
 		
-		width = TextButton.WIDTH * 2 + 20;
-		lines = RenderAssistant.wrap(message, messageFont, width - 60);
-		height = 90 + lines.length * FontAssistant.getFont(messageFont).getHeight();
-		x = Display.getWidth() / 2 - width / 2;
-		y = Display.getHeight() / 2 - height / 2;
-		
-		if (buttons.length > 0) height += TextButton.HEIGHT;
-		
-		if (buttons.length > 2) throw new InvalidParameterException("Max button count is 2");
-		this.buttons = new TextButton[buttons.length];
-		if (buttons.length == 1) this.buttons[0] = new TextButton(x + width / 2, y + height - 20 - TextButton.HEIGHT, buttons[0]);
-		else
-		{
-			this.buttons[0] = new TextButton(x + width / 4 + 10, y + height - 20 - TextButton.HEIGHT, buttons[0]);
-			this.buttons[1] = new TextButton(x + width / 4 * 3 - 10, y + height - 20 - TextButton.HEIGHT, buttons[1]);
-		}
-		for (TextButton b : this.buttons)
-		{
-			content.add(b);
-		}
-		container = new Container(x, y, width, height);
-		container.border = false;
-		content.add(container);
+		init();
 	}
 	
 	@Override
@@ -102,7 +84,34 @@ public class Dialog extends Scene
 	
 	@Override
 	public void init()
-	{}
+	{
+		title = rawTitle;
+		centered = true;
+		
+		width = TextButton.WIDTH * 2 + 20;
+		lines = RenderAssistant.wrap(rawMessage, messageFont, width - 60);
+		height = 90 + lines.length * FontAssistant.getFont(messageFont).getHeight();
+		x = Display.getWidth() / 2 - width / 2;
+		y = Display.getHeight() / 2 - height / 2;
+		
+		if (rawActions.length > 0) height += TextButton.HEIGHT;
+		
+		if (rawActions.length > 2) throw new InvalidParameterException("Max button count is 2");
+		buttons = new TextButton[rawActions.length];
+		if (rawActions.length == 1) buttons[0] = new TextButton(x + width / 2, y + height - 20 - TextButton.HEIGHT, rawActions[0]);
+		else
+		{
+			buttons[0] = new TextButton(x + width / 4 + 10, y + height - 20 - TextButton.HEIGHT, rawActions[0]);
+			buttons[1] = new TextButton(x + width / 4 * 3 - 10, y + height - 20 - TextButton.HEIGHT, rawActions[1]);
+		}
+		for (TextButton b : buttons)
+		{
+			content.add(b);
+		}
+		container = new Container(x, y, width, height);
+		container.border = false;
+		content.add(container);
+	}
 	
 	public void addComponent(IGuiElement g)
 	{
