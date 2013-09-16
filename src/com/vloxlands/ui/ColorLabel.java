a932b8e92ee19a16a503daafb8c84c64534224f0
Ichmed <mijomeyer@t-online.de>
2013-09-16 20:51:20 +0200

    ehhhhh

--- src/com/vloxlands/ui/ColorLabel.java
+++ src/com/vloxlands/ui/ColorLabel.java
@@ -25,13 +25,15 @@ public class ColorLabel extends Container
 	{
 		super.render();
 		glEnable(GL_BLEND);
+		glEnable(GL_TEXTURE_2D);
 		int fac = 8;
 		TextureImpl.bindNone();
-		glColor3f(1, 0, 0);
+//		glColor3f(1, 0, 0);
+//		RenderAssistant.drawRect(x + fac, y + fac, width - fac * 2, height - fac * 2);
+//		glColor4f(1, 1, 1, 0.5f);
+		RenderAssistant.bindTexture("/graphics/textures/ui/colorMod.png");
 		RenderAssistant.drawRect(x + fac, y + fac, width - fac * 2, height - fac * 2);
 		glColor3f(1, 1, 1);
-		// RenderAssistant.bindTexture("/graphics/textures/ui/colorMod.png");
-		RenderAssistant.drawRect(x + fac, y + fac, width - fac * 2, height - fac * 2);
 		glDisable(GL_BLEND);
 	}
 }
