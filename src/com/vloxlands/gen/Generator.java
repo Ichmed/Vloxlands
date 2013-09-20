e9e946d77ce7b461a736658133a33103dbd0a418
Dakror <mail@dakror.de>
2013-09-20 15:28:32 +0200

    Moved GenerationHelper function

--- src/com/vloxlands/gen/Generator.java
+++ src/com/vloxlands/gen/Generator.java
@@ -105,4 +105,18 @@ public abstract class Generator
 		}
 		return baos.toByteArray();
 	}
+	
+	public static void fillSphere(Island island, int x, int y, int z, int rad, int id)
+	{
+		for (int i = 0; i < rad * 2; i++)
+		{
+			for (int j = 0; j < rad * 2; j++)
+			{
+				for (int k = 0; k < rad * 2; k++)
+				{
+					if (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) <= rad) island.setVoxel(x + i, y + j, z + k, (byte) id);
+				}
+			}
+		}
+	}
 }
